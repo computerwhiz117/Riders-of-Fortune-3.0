911979adebb6998d3571e5947eb4b32a45bb7e20
computerwhiz117 <computerwhiz117@gmail.com>
2019-04-26 17:47:09 -0400

    Changes in code

--- Riders of Fortune 3.0/Riders of Fortune 3.0.cpp
+++ Riders of Fortune 3.0/Riders of Fortune 3.0.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Enemy {
@@ -27,7 +28,7 @@ int main()
 	cout << "Welcome to Riders of Fortune 3.0, the open source fantasy text based adventure game." << endl;
 	cout << "1. Start the game" << endl;
 	cout << "2. Load previous save" << endl;
-	cout << "2. Quit the game" << endl;
+	cout << "3. Quit the game" << endl;
 	cin >> i;
 
 	if (1 == 1)
@@ -75,7 +76,76 @@ int main()
 				{
 					cout << "You have defeated the Reaper" << endl;
 					cout << "Your XP ranked up to " << Yarra.PlayerXP << endl;
+
+					cout << "Make your choice: " << endl;
+
+					cout << "1. Travel to another space on the board " << endl;
+					cout << "2. Dismount and explore the current space" << endl;
+					cout << "3. Save your game" << endl;
+					cin >> i;
+
+					if (i == 1)
+					{
+						Enemy Troll;
+
+						double volume = 0.0;
+
+						Troll.getHP = 2.0;
+
+						Troll.setHP = 5.0;
+
+						volume = Troll.getHP * Troll.setHP;
+
+						cout << "Troll: " << volume << endl;
+
+						cout << "I am the brute troll, I will not let you through" << endl;
+
+						cin >> battle;
+
+						if (battle > volume)
+
+							cout << "You hit the Troll" << endl;
+							
+						cin >> battle;
+
+						if (volume == 0)
+						{
+							Player Yarra;
+
+							Yarra.PlayerXP = 0.0;
+
+							Yarra.PlayerXP = volume - battle;
+
+							cout << "You hit the Troll" << endl;
+
+							cin >> battle;
+
+							cout << "You have defeated the Troll" << endl;
+							cout << "Your XP ranked up to" << Yarra.PlayerXP << endl;
+						
+							cout << "Make your choice: " << endl;
+							cout << "1. Travel and explore the current space" << endl;
+							cout << "2. Dismount and explore the current space" << endl;
+							cout << "3. Save your game" << endl;
+							cin >> i;
+
+							
+						}
+						else
+						{
+							cout << "I hit you with my mallot and I killed you" << endl;
+							cout << "GAME OVER!!!!!!!!!!";
+
+							
+						}
+					}
+					else
+					{
+						cout << "I hit you with my mallot and I killed you" << endl;
+						cout << "GAME OVER!!!!!!!!!!";
+					}
 				}
+
 				else
 				{
 					cout << "I slashed you in half and now your soul is mine" << endl;
@@ -90,6 +160,169 @@ int main()
 
 		}
 	}
+
+	else if (i == 2)
+	{
+		cout << "You found a silver sword" << endl;
+		cout << "Do you want to equip it?" << endl;
+		cout << "1. Yes" << endl;
+		cout << "2. No" << endl;
+		cin >> i;
+
+		if (i == 1)
+		{
+			vector <string> WeaponsLimit;
+
+			WeaponsLimit.push_back("Silver sword");
+
+			cout << "You kept " << WeaponsLimit[i] << endl;
+
+			cout << "Make your choice: " << endl;
+
+			cout << "1. Travel to another space on the board" << endl;
+			cout << "2. Dismount and explore the current space" << endl;
+			cout << "3. Save your game" << endl;
+			cin >> i;
+
+			if (i == 1)
+			{
+				Enemy Reaper;
+				double volume = 0.0;
+
+				Reaper.getHP = 2.0;
+
+				Reaper.setHP = 5.0;
+
+
+				volume = Reaper.getHP * Reaper.setHP;
+
+				cout << "Reaper: " << volume << endl;
+
+				cout << "I am the Raging Reaper, I seek for new souls which yours will be mine." << endl;
+
+				cin >> battle;
+				if (battle > volume)
+				{
+
+
+					Player Yarra;
+
+					Yarra.PlayerXP = 0.0;
+
+					Yarra.PlayerXP = volume - battle;
+
+					cout << "You hit the Reaper" << endl;
+
+					cin >> battle;
+
+					if (volume == 0)
+					{
+						cout << "You have defeated the Reaper" << endl;
+						cout << "Your XP ranked up to " << Yarra.PlayerXP << endl;
+
+						cout << "Make your choice: " << endl;
+
+						cout << "1. Travel to another space on the board " << endl;
+						cout << "2. Dismount and explore the current space" << endl;
+						cout << "3. Save your game" << endl;
+						cin >> i;
+
+						if (i == 1)
+						{
+							Enemy Troll;
+
+							double volume = 0.0;
+
+							Troll.getHP = 2.0;
+
+							Troll.setHP = 5.0;
+
+							volume = Troll.getHP * Troll.setHP;
+
+							cout << "Troll: " << volume << endl;
+
+							cout << "I am the brute troll, I will not let you through" << endl;
+
+							cin >> battle;
+
+							if (battle > volume)
+
+								cout << "You hit the Troll" << endl;
+
+							cin >> battle;
+
+							if (volume == 0)
+							{
+								Player Yarra;
+
+								Yarra.PlayerXP = 0.0;
+
+								Yarra.PlayerXP = volume - battle;
+
+								cout << "You hit the Troll" << endl;
+
+								cin >> battle;
+
+								cout << "You have defeated the Troll" << endl;
+								cout << "Your XP ranked up to" << Yarra.PlayerXP << endl;
+
+								cout << "Make your choice: " << endl;
+								cout << "1. Travel and explore the current space" << endl;
+								cout << "2. Dismount and explore the current space" << endl;
+								cout << "3. Save your game" << endl;
+								cin >> i;
+
+								if (i == 1)
+								{
+									vector<int> vec;
+									int i;
+
+									cout << "I am the Mystic Blacksmith" << endl;
+									cout << "You can leave your weapons here and I'll save them for later when you need them" << endl;
+
+									cout << "Weapons: " << WeaponsLimit[i] << endl;
+
+
+
+
+									for (i = 0; i < 5; i++) {
+										cout << "I will store your weapons " << endl;
+
+										vector<int>::iterator v = vec.begin();
+										while ((v != vec.end()))
+										{
+											cout << "You have " << *v << "weapons" << endl;
+											v++;
+										}
+
+									}
+								}
+
+							}
+							else
+							{
+								cout << "I hit you with my mallot and I killed you" << endl;
+								cout << "GAME OVER!!!!!!!!!!";
+							}
+						}
+
+						else
+						{
+							cout << "I slashed you in half and now your soul is mine" << endl;
+							cout << "GAME OVER!!!!!!!!!!!" << endl;
+						}
+					}
+					else
+					{
+						cout << "I slashed you in half and now your soul is mine" << endl;
+						cout << "GAME OVER!!!!!!!!!!!" << endl;
+					}
+
+				}
+			}
+		}
+	}
+
 	else
 	{
 		cout << "Good bye";
@@ -97,14 +330,3 @@ int main()
 
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
